bd3296f90ef5f29042c5264bf8819250f2471bf3
GabusArt <gabusart19@gmail.com>
2022-08-04 23:38:10 +0200

    Update UI and added one biome SpriteSheets

--- Assets/Scripts/GameButton.cs
+++ Assets/Scripts/GameButton.cs
@@ -1,18 +1,29 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Action<bool> OnButtonDown;
+    [SerializeField] Sprite buttonUp;
+     [SerializeField] Sprite buttonDown;
+
 
     public void OnPointerDown(PointerEventData eventData)
     {
         OnButtonDown?.Invoke(true);
+        ChangeSprite(buttonDown);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         OnButtonDown?.Invoke(false);
+        ChangeSprite(buttonUp);
+    }
+
+    private void ChangeSprite(Sprite currentsprite)
+    {
+        GetComponent<Image>().sprite = currentsprite;
     }
 }
